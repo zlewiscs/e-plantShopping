23fd37159f28a21d219af96461009e78d7d5f34a
Lewis Zhang <liang.lewis.z@gmail.com>
2024-07-08 04:43:21 -0400

    Implemented cartCount feature

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,6 +2,16 @@ import React, { useState,useEffect } from 'react';
 import {addItem} from './CreatSlice.jsx'
 import './ProductList.css'
 function ProductList() {
+    const [cartCount, setCartCount] = useState(0);
+    const [addedToCart, setAddedToCart] = useState({});
+    const handleAddToCart = (product) => {
+     dispatch(addItem(product));
+     setCartCount(prevCartCount => prevCartCount + 1)
+     setAddedToCart((prevState) => ({
+        ...prevState,
+        [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+      }));
+   };
   
     const plantsArray = [
         {
@@ -231,15 +241,6 @@ function ProductList() {
     textDecoration: 'none',
    }
 
-   const [addedToCart, setAddedToCart] = useState({});
-   const handleAddToCart = (product) => {
-    dispatch(addItem(product));
-    setAddedToCart((prevState) => ({
-       ...prevState,
-       [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
-     }));
-  };
-
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -257,7 +258,7 @@ function ProductList() {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" style={styleA}>Plants</a></div>
-                <div> <a href="#" style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> <a href="#" style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>({cartCount})</h1></a></div>
             </div>
         </div>
 
@@ -270,7 +271,7 @@ function ProductList() {
                     <div className="product-card" key={plantIndex}>
                         <img className="product-image" src={plant.image} alt={plant.name} />
                         <div className="product-title">{plant.name}</div>
-                        <button  onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                        <button  onClick={() => handleAddToCart(plant)} disabled={addedToCart[plant.id]}>Add to Cart</button>
                     </div>
                     ))}
                 </div>
