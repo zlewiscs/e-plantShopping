af8d9582758909c0d7a5af31d5e510a09f57944c
Lewis Zhang <liang.lewis.z@gmail.com>
2024-07-08 06:19:47 -0400

    completed add and remove item functions, optimised updateQuantity for readability by using diff rather than 2 separate functions

--- src/CreatSlice.jsx
+++ src/CreatSlice.jsx
@@ -1,32 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const CreatSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-        const { name, image, cost } = action.payload;
-        const existingItem = state.items.find(item => item.name === name);
-        if (existingItem) {
-            existingItem.quantity++;
-        } else {
-            state.items.push({ name, image, cost, quantity: 1 });
-        }
-    },
+const CreatSlice = createSlice({
+    name: 'cart',
+    initialState : { items: [] },
+    reducers: {
+        // If an item is already in the cart, increase the count
+        // If not add the item to the cart and set its count to 1
+        addItem: (state, action) => {
+            const { id, name, image, cost } = action.payload;
+            const existingItem = state.items.find((item) => item.id === id);
+            if (existingItem) {
+                existingItem.quantity++;
+            } else {
+                state.items.push({ id, name, image, cost, quantity: 1 });
+            }
+        },
+        removeItem: (state, action) => {
+            const id = action.payload;
+            state.items = state.items.filter((item) => item.id !== id);
+        },
 
-    removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
+        // Destructure the payload before finding the item to update, use diff which can be + or -
+        // to indicate + or -
+        updateQuantity: (state, action) => {
+            const { plant, diff } = action.payload;
+            let itemToUpdate = state.items.find((item) => item.id === plant.id);
+            if (itemToUpdate && itemToUpdate.quantity + diff > 0) {
+                itemToUpdate.quantity += diff;
+            }
+        },
     },
-    updateQuantity: (state, action) => {
-        const { name, quantity } = action.payload;
-        const itemToUpdate = state.items.find(item => item.name === name);
-        if (itemToUpdate) {
-            itemToUpdate.quantity = quantity;
-        }
-    },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CreatSlice.actions;
